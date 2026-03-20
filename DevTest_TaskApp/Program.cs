14a5effbe89e9a721af58f26fed962dea792dfbe
Waqas <Waqas@LAPTOP-9TCLKQC4>
2026-03-20 17:59:07 +0000

    Add project files.

---
+++ DevTest_TaskApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using DevTest_TaskApi.Data;
+using DevTest_TaskApi.Services;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DevTest_TaskApiContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevTest_TaskApiContext") ?? throw new InvalidOperationException("Connection string 'DevTest_TaskApiContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ITasksService, TasksService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ DevTest_TasksWeb/Models/Tasks.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevTest_TasksWeb.Models
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        public Task_Status Task_Status { get; set; }
+        [Required]
+        public DateTime DueAt { get; set; }
+    }
+    public enum Task_Status
+    {
+        NotStarted = 0,
+        InProgress = 1,
+        Completed = 2
+    }
+}
